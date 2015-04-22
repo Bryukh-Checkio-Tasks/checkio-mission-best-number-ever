9f25c6ab6d8269b1efda622b8335a2744624c1fa
Valentin Bryukhanov <bvv.mag@gmail.com>
2015-04-22 14:31:23 +0300

    Fix js

--- editor/animation/init.js
+++ editor/animation/init.js
@@ -78,7 +78,7 @@ requirejs(['ext_editor_1', 'jquery_190', 'raphael_210', 'snap.svg_030'],
 
                 //if you need additional info from tests (if exists)
                 var explanation = data.ext["explanation"];
-                $content.find('.output').html('&nbsp;Your result:&nbsp;' + JSON.stringify(userResult));
+                $content.find('.output').html('&nbsp;Your result:&nbsp;' + userResult);
                 if (!result) {
                     $content.find('.answer').html('Right result:&nbsp;' + JSON.stringify(rightResult));
                     $content.find('.answer').addClass('error');
